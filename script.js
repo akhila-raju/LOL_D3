657ed7e8d75a1927f9b003ef20371ae69f9f9b73
Jimmy Yin <jimmy.yin@berkeley.edu>
2014-10-14 15:47:07 -0700

    moved sag

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ var margin = {top: 20, right: 30, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 var xScale = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
 
@@ -140,10 +147,7 @@ function dayOfWeek(ms){
 
 // Visualization 
 function begin(){
-	var svg = d3.select("body")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
+	
             
     
 }
